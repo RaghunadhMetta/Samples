e1666453c877b8c0005cd149c37aad25486abb14
Raghu <raghunadh.metta@samsclub.com>
2020-02-21 00:59:56 -0600

    minor Code Refactor

--- src/com/company/ReverseString/ReverseString.java
+++ src/com/company/ReverseString/ReverseString.java
@@ -1,6 +1,6 @@
-package com.company;
+package com.company.ReverseString;
 
-public class Main {
+public class ReverseString {
 
     public static void main(String[] args) {
         // write your code here
