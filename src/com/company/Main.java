d91d50ca3460a466fc0f21da8334f265a1a39fa8
Raghu <raghunadh.metta@samsclub.com>
2020-02-21 00:57:04 -0600

    First commit to repository.

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String name = "abcde";
+        char[] chars = name.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+
+        }
+
+        System.out.println(chars);
+    }
+}
